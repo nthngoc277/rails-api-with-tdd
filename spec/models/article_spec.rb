41904ca084e755cd7ce59cc6730a1d26f5955360
Ruby <ngoc@tinypulse.com>
2018-10-08 06:39:22 +0700

    Implement recent articles

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  describe 'validations' do
+  describe '#validations' do
     it 'tests that factory is valid' do
       expect(build :article).to be_valid
     end
@@ -29,6 +29,16 @@ RSpec.describe Article, type: :model do
       invalid_article = build :article, slug: article.slug
       expect(invalid_article).not_to be_valid
       expect(invalid_article.errors.messages[:slug]).to include("has already been taken")
+    end
+  end
+
+  describe '.recent' do
+    it 'recent article in the descending order by created at' do
+      old_article = create :article
+      new_article = create :article
+      expect(described_class.recent).to eq [new_article, old_article]
+      old_article.update_column :created_at, Time.now
+      expect(described_class.recent).to eq [old_article, new_article]
     end
   end
 end
