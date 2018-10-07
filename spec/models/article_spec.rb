311fc00e96ee0a31534f67c29f517598cf80c7d5
Ruby <ngoc@tinypulse.com>
2018-10-07 23:25:56 +0700

    Adding config for FactoryBot

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,30 +3,30 @@ require 'rails_helper'
 RSpec.describe Article, type: :model do
   describe 'validations' do
     it 'tests that factory is valid' do
-      expect(FactoryBot.build :article).to be_valid
+      expect(build :article).to be_valid
     end
 
     it 'validates the presence of title' do
-      article = FactoryBot.build :article, title: ''
+      article = build :article, title: ''
       expect(article).not_to be_valid
       expect(article.errors.messages[:title]).to include("can't be blank")
     end
 
     it 'validates the presence of content' do
-      article = FactoryBot.build :article, content: ''
+      article = build :article, content: ''
       expect(article).not_to be_valid
       expect(article.errors.messages[:content]).to include("can't be blank")
     end
 
     it 'validates the presence of slug' do
-      article = FactoryBot.build :article, slug: ''
+      article = build :article, slug: ''
       expect(article).not_to be_valid
       expect(article.errors.messages[:slug]).to include("can't be blank")
     end
 
     it 'validates the uniqueness of slug' do
-      article = FactoryBot.create :article
-      invalid_article = FactoryBot.build :article, slug: article.slug
+      article = create :article
+      invalid_article = build :article, slug: article.slug
       expect(invalid_article).not_to be_valid
       expect(invalid_article.errors.messages[:slug]).to include("has already been taken")
     end
