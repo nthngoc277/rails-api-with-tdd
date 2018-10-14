c1d67999f345989984e67655dff1c7b20e4123a3
Ruby <ngoc@tinypulse.com>
2018-10-15 00:48:51 +0700

    Implement success case of logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,24 @@ class ApplicationController < ActionController::API
 
   rescue_from UserAuthenticator::AuthenticationError, with: :authentication_error
   rescue_from AuthorizationError, with: :forbidden_request_error
+
+  before_action :authorize!
+
   private 
+
+  def access_token
+    provided_token = request.authorization&.gsub(/\ABearer\s/, '')
+    @access_token = AccessToken.find_by_token(provided_token)
+  end
+
+  def current_user
+    @current_user = access_token&.user
+  end
+
+  def authorize!
+    raise AuthorizationError unless current_user
+  end
+
   def authentication_error
     errors = {  "status": "401",
       "source": { "pointer": "/code" },
