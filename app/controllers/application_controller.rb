9e3996ec6ba3865862970381f93b472e50366c94
Ruby <ngoc@tinypulse.com>
2018-10-14 23:53:57 +0700

    Implement logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::API
-  rescue_from UserAuthenticator::AuthenticationError, with: :authentication_error
+  class AuthorizationError < StandardError; end
 
+  rescue_from UserAuthenticator::AuthenticationError, with: :authentication_error
+  rescue_from AuthorizationError, with: :forbidden_request_error
   private 
   def authentication_error
     errors = {  "status": "401",
@@ -9,5 +11,15 @@ class ApplicationController < ActionController::API
       "detail": "You mush provide valid code in order to exchange it for token"
     }
     render json: {'errors': [errors]}, status: 401
+  end
+
+  def forbidden_request_error
+    errors = {
+      'status' => '403',
+      'source' => { 'pointer' => 'header/authorization' },
+      'title' => 'Not authorized',
+      'detail' => 'You have no right to access this resource'
+    }
+    render json: {'errors': [errors]}, status: 403
   end
 end
--- spec/controllers/access_tokens_controller_spec.rb
+++ spec/controllers/access_tokens_controller_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe AccessTokensController, type: :controller do
-  describe '.create' do
+  describe 'POST .create' do
     shared_examples_for "unauthorized_request" do
-      let(:errors) do
+      let(:authorization_error) do
         {
           "status"=>"401",
           "source"=>{ "pointer"=>"/code" },
@@ -19,7 +19,7 @@ RSpec.describe AccessTokensController, type: :controller do
 
       it 'returns proper error body' do
         subject
-        expect(json['errors']).to include(errors)
+        expect(json['errors']).to include(authorization_error)
       end
     end
 
@@ -62,6 +62,37 @@ RSpec.describe AccessTokensController, type: :controller do
           { 'token' => user.access_token.token }
         )
       end
+    end
+  end
+
+  describe 'DELETE .destroy' do
+    shared_examples_for 'forbidden_requests' do
+      let(:forbidden_error) do
+        {
+          'status' => '403',
+          'source' => { 'pointer' => 'header/authorization' },
+          'title' => 'Not authorized',
+          'detail' => 'You have no right to access this resource'
+        }
+      end
+
+      it 'returns 403 status code' do
+        subject
+        expect(response).to have_http_status(:forbidden)
+      end
+
+      it 'should return proper json error' do
+        subject
+        expect(json['errors']).to eq([forbidden_error])
+      end
+    end
+
+    context 'when invalid request' do
+      subject { delete :destroy }
+      it_behaves_like 'forbidden_requests'
+    end
+
+    context 'when valid request' do
     end
   end
 end
